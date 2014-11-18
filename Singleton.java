04f26a7ddd493baa579dbfac18fd64681e1c1ec1
ngoyal <naveen.goyal@cloudsmartz.net>
2014-11-18 16:47:43 +0530

    first commit

---
+++ Singleton.java
@@ -0,0 +1,26 @@
+package conversion.into.different.forms.controller;
+
+public class Singleton{
+	
+private static volatile Singleton _instance; //volatile variable
+
+public static Singleton getInstance(int priorityCount) throws InterruptedException{
+
+	Thread.currentThread().setPriority(++priorityCount);
+   if(_instance == null){
+            synchronized(Singleton.class){
+              if(_instance == null)
+              _instance = new Singleton();
+              System.out.println("holds lock======"+Thread.holdsLock(_instance));
+              System.out.println("instance is created now");
+              Thread.currentThread().yield();
+            }
+
+   }else{
+	   System.out.println("holds lock=in else condition====="+Thread.holdsLock(_instance));
+	   System.out.println("instance is already created");
+   }
+   return _instance;
+	}
+}
+
